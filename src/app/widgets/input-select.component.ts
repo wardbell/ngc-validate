517b31f5c0368928b23e5e149a2a9e1aeb291d00
Ward Bell <wardbell@hotmail.com>
2022-07-16 19:13:14 -0700

    refactor widgets to use InputBaseComponent base class

--- src/app/widgets/input-select.component.ts
+++ src/app/widgets/input-select.component.ts
@@ -1,15 +1,14 @@
-import { Component, AfterViewInit, ElementRef, EventEmitter, HostBinding, Input, OnChanges, OnDestroy, OnInit, Optional, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, NgModel, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
-import { Subscription } from 'rxjs';
 
-import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
-import { formContainerViewProvider, Indexable } from '@core';
-import { FormHooks, nameCounter, NgModelOptions, SelectOption, trim } from '@app/widgets/interfaces';
-import { FormValidationScopeDirective, ValidationContext } from '@validation';
-import { InputErrorComponent } from '@app/widgets';
+import { formContainerViewProvider } from '@core';
 import { FormFieldNgModelDirective } from '@validation/form-field-ng-model.directive';
+import { FormValidationScopeDirective } from '@validation';
+import { InputBaseComponent } from './input-base.component';
+import { InputErrorComponent } from './input-error.component';
+import { SelectOption } from './interfaces';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -29,7 +28,7 @@ import { FormFieldNgModelDirective } from '@validation/form-field-ng-model.direc
       [(ngModel)]="value"
       [ngModelOptions]="ngModelOptions!"
       (keyup.esc)="escaped()"
-      #select #ngModel="ngModel">
+      #input #ngModel="ngModel">
         <option *ngIf="showPlaceholderOption" [value]="value" disabled>
           {{ placeholderOption }}
         </option>
@@ -41,100 +40,17 @@ import { FormFieldNgModelDirective } from '@validation/form-field-ng-model.direc
   <input-error [control]="ngModel.control"></input-error>
   `,
 })
-export class InputSelectComponent implements AfterViewInit, OnInit, OnChanges, OnDestroy  {
-  @HostBinding('class') get klass() { return this.className; }
-  @Input() context?: ValidationContext;
-  @Input()
-  get disabled(): boolean { return this._disabled; };
-  set disabled(value: BooleanInput) { this._disabled = coerceBooleanProperty(value); }
-  private _disabled = false;
-
-  @Input() field?: string;
-  @Input() group?: string;
-  @Input() label?: string;
-  // eslint-disable-next-line @angular-eslint/no-input-rename
-  @Input('model') inputModel?: Indexable;
-  @Input() modelType?: string;
-  @Input() name?: string;
+export class InputSelectComponent extends InputBaseComponent  {
   @Input() options: SelectOption[] = [];
+
   /** Display this text as the (disabled) first option when the model.field is "empty". */
   @Input() placeholderOption = '';
 
-  @Input() updateOn?: FormHooks;
-
-  /** Emit when the field value changes. */
-  @Output() changed = new EventEmitter<string | null>();
-
-  @ViewChild('select') selectElRef?: ElementRef;
-  @ViewChild('ngModel') ngModel?: NgModel;
-
-  protected className: string;
-  private hostEl: HTMLInputElement = this.hostElRef.nativeElement;
-  model?: Indexable;
-  protected ngModelOptions?: NgModelOptions;
-  originalValue: any | null = null;
-  scopeSub?: Subscription;
-
-  get value() {
-    return this.model![this.field!];
-  }
-
-  set value(val: any | null) {
-    if (this.field) {
-      this.model![this.field] = val;
-    }
-    this.changed.emit(val);
-  }
   constructor(
-    @Optional() private formValidationScope: FormValidationScopeDirective,
-    private hostElRef: ElementRef
+    @Optional() formValidationScope: FormValidationScopeDirective,
+    hostElRef: ElementRef
   ) {
-    // Apply the class attribute on the host; if none, apply app standard CSS classes
-    this.className = this.hostEl.className || "col full-width";
-  }
-
-  ngOnChanges(): void {
-    // only care about inputModel and updateOn changes; changes to other input vars are ignored.
-    // inputModel trumps validation scope model
-    this.model = this.inputModel ?? this.formValidationScope?.model ?? {};
-    if (this.updateOn) {
-      this.ngModelOptions = { updateOn: this.updateOn };
-    }
-  }
-
-  ngOnInit(): void {
-    this.model = this.model ?? this.formValidationScope?.model ?? {};
-    this.field = this.field || this.name;
-    this.name = this.name || `${(this.field || '')}$${nameCounter.next}`;
-    this.originalValue = trim(this.model ? this.model[this.field!] : null);
-    if (this.updateOn) {
-      this.ngModelOptions = { updateOn: this.updateOn };
-    }
-  }
-
-  ngAfterViewInit() {
-    if (this.hostEl.hasAttribute('required')) {
-      // Pass it along to the nested input element
-      // Wait a tick to bypass ExpressionChangedAfterItHasBeenCheckedError
-      setTimeout(() => {
-        // Adding the required validator triggers Angular Material
-        // to add the required attribute and update the label with the required marker.
-        this.ngModel!.control.addValidators(Validators.required);
-        this.ngModel!.control.updateValueAndValidity({ onlySelf: true });
-      });
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.scopeSub?.unsubscribe();
-  }
-
-  /** User pressed escape. Restores original value */
-  protected escaped() {
-    const currentValue = this.selectElRef!.nativeElement.value;
-    if (currentValue !== this.originalValue) {
-      this.value = this.originalValue;
-    }
+    super(formValidationScope, hostElRef);
   }
 
   get showPlaceholderOption() {
--- src/app/widgets/input-text.component.ts
+++ src/app/widgets/input-text.component.ts
@@ -1,15 +1,13 @@
-import { Component, AfterViewInit, ElementRef, EventEmitter, HostBinding, Input, OnChanges, OnDestroy, OnInit, Optional, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormsModule, NgModel, Validators } from '@angular/forms';
-import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
+import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
-import { Subscription } from 'rxjs';
 
-import { formContainerViewProvider, Indexable } from '@core';
-import { FormHooks, nameCounter, NgModelOptions, trim } from '@app/widgets/interfaces';
-import { FormValidationScopeDirective, ValidationContext } from '@validation';
-import { InputErrorComponent } from '@app/widgets';
+import { formContainerViewProvider } from '@core';
 import { FormFieldNgModelDirective } from '@validation/form-field-ng-model.directive';
+import { FormValidationScopeDirective } from '@validation';
+import { InputBaseComponent } from './input-base.component';
+import { InputErrorComponent } from './input-error.component';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -37,100 +35,12 @@ import { FormFieldNgModelDirective } from '@validation/form-field-ng-model.direc
   <input-error [control]="ngModel.control"></input-error>
   `,
 })
-export class InputTextComponent implements AfterViewInit, OnInit, OnChanges, OnDestroy {
-  @HostBinding('class') get klass() { return this.className; }
-  @Input() context?: ValidationContext;
-  @Input()
-  get disabled(): boolean { return this._disabled; };
-  set disabled(value: BooleanInput) { this._disabled = coerceBooleanProperty(value); }
-  private _disabled = false;
-
-  @Input() field?: string;
-  @Input() group?: string;
-  @Input() label?: string;
-  // eslint-disable-next-line @angular-eslint/no-input-rename
-  @Input('model') inputModel?: Indexable;
-  @Input() modelType?: string;
-  @Input() name?: string;
-  @Input() placeholder?: string;
-  @Input() type = 'text';
-  @Input() updateOn?: FormHooks;
-
-  /** Emit when the field value changes. */
-  @Output() changed = new EventEmitter<string | null>();
-
-  @ViewChild('input') inputElRef?: ElementRef;
-  @ViewChild('ngModel') ngModel?: NgModel;
-
-  protected className: string;
-  control?: FormControl;
-  private hostEl: HTMLInputElement = this.hostElRef.nativeElement;
-  model?: Indexable;
-  protected ngModelOptions?: NgModelOptions;
-  originalValue: any | null = null;
-  scopeSub?: Subscription;
-
-  get value() {
-    return this.model![this.field!];
-  }
-
-  set value(val: string) {
-    // TODO: coerce value to type of original value
-    val = trim(val);
-    if (this.field) {
-      this.model![this.field] = val;
-    }
-    this.changed.emit(val);
-  }
-
+export class InputTextComponent extends InputBaseComponent {
   constructor(
-    @Optional() private formValidationScope: FormValidationScopeDirective,
-    private hostElRef: ElementRef
+    @Optional() formValidationScope: FormValidationScopeDirective,
+    hostElRef: ElementRef
   ) {
-    // Apply the class attribute on the host; if none, apply app standard CSS classes
-    this.className = this.hostEl.className || "col full-width";
-    this.scopeSub = formValidationScope?.changed.subscribe(_ => this.ngOnChanges()) // might reset the model
-  }
-
-  ngOnChanges(): void {
-    // only care about inputModel and updateOn changes; changes to other input vars are ignored.
-    // inputModel trumps validation scope model
-    this.model = this.inputModel ?? this.formValidationScope?.model ?? {};
-    if (this.updateOn) {
-      this.ngModelOptions = { updateOn: this.updateOn };
-    }
-  }
-
-  ngOnInit(): void {
-    this.field = this.field || this.name;
-    this.name = this.name || `${(this.field ?? '')}$${nameCounter.next}`;
-    this.originalValue = trim(this.model![this.field!]);
-  }
-
-  ngAfterViewInit() {
-    this.control = this.ngModel!.control;
-    if (this.hostEl.hasAttribute('required')) {
-      // Pass it along to the nested input element
-      // Wait a tick to bypass ExpressionChangedAfterItHasBeenCheckedError
-      setTimeout(() => {
-        // Adding the required validator triggers Angular Material
-        // to add the required attribute and update the label with the required marker.
-        this.control!.addValidators(Validators.required);
-        this.control!.updateValueAndValidity({ onlySelf: true });
-      });
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.scopeSub?.unsubscribe();
-  }
-
-  /** User pressed escape. Restores original value */
-  protected escaped() {
-    const elementValue = this.inputElRef!.nativeElement.value;
-    if (elementValue !== this.originalValue) {
-      this.value = this.originalValue;
-    }
+    super(formValidationScope, hostElRef)
   }
 
   protected onBlur() {
